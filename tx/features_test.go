fd4b3341b5667740073876410ae05ed7f903667c
miniBamboo <33890091+miniBamboo@users.noreply.github.com>
2021-01-29 14:18:21 +0800

    Add files via upload

---
+++ tx/features_test.go
@@ -0,0 +1,24 @@
+package tx_test
+
+import (
+	"testing"
+
+	"github.com/miniBamboo/luckyshare/tx"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFeatures(t *testing.T) {
+	var f tx.Features
+
+	assert.Zero(t, f)
+	assert.False(t, f.IsDelegated())
+
+	f.SetDelegated(true)
+	assert.True(t, f.IsDelegated())
+
+	f.SetDelegated(false)
+	assert.False(t, f.IsDelegated())
+
+	f.SetDelegated(false)
+	assert.False(t, f.IsDelegated())
+}
